f4a8b3f2c43f420e35188f5a2eae0bdf55585d50
GuTianqi <ntpeter164@126.com>
2020-02-10 22:22:44 +0800

    merge

---
+++ Assets/Scripts/Managers/SaveManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class SaveManager:MonoBehaviour
+{
+    private FishDataList capturedFishes;
+    [SerializeField] private string savePath = "save1";
+    /*
+    public static SaveManager Instance 
+    {
+        get 
+        {
+            if (Instance == null)
+            { Instance = new SaveManager(); }
+            return Instance;
+        }
+        private set { Instance = value; }
+    }
+    */
+    private void Start()
+    {
+        Load(savePath);
+        Save();
+    }
+    public bool Load(string path)
+    {
+        if(!File.Exists(path))
+        {
+            Debug.LogError("cant load");
+            return false;
+        }
+        string data = File.ReadAllText(path);
+        capturedFishes = JsonUtility.FromJson<FishDataList>(data);
+        return true;
+    }
+    public void Save()
+    {
+        string save = JsonUtility.ToJson(capturedFishes);
+        Debug.Log(save);
+        StreamWriter writer = new StreamWriter(savePath);
+        writer.Write(save);
+        writer.Close();
+    }
+}
--- Assets/Scripts/Scanning.cs
+++ Assets/Scripts/Scanning.cs
@@ -4,7 +4,40 @@ using UnityEngine;
 
 public class Scanning : MonoBehaviour
 {
+    public float ScanTime;
+    private float curValue;
+    private RaycastHit hit;
     private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if(Physics.Raycast(this.transform.position, transform.forward, out hit))
+            {
+                OnScan(hit);
+            }
+            else
+            {
+                OutOfScan();//value drop
+            }
+
+        }
+    }
+    private void OnScan(RaycastHit hit)
+    {
+        curValue += Time.deltaTime;
+        if (curValue > ScanTime)
+        {
+            curValue = 0;
+            Scan();
+        }
+    }
+
+    private void OutOfScan()
+    {
+        curValue -= Time.deltaTime;
+    }
+
+    private void Scan()
     {
     }
 }
