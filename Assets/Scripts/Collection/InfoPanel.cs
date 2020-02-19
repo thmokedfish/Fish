944b3d4f01bfe844871036d45e9841a31922b3de
JiaHanXiao <JianHanXiao@outlook.com>
2020-02-19 20:24:21 +0800

    show

---
+++ Assets/Scripts/Collection/InfoPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoPanel : MonoBehaviour
+{
+    public Text name;
+    public Text rare;
+    public Text info;
+    public Button button;
+    public GameObject MainPanel;
+    public GameObject Panel;
+    public void SetText(string n, string r, string i)
+    {
+        name.text = n;
+        rare.text = r;
+        info.text = i;
+        button.onClick.AddListener(Return);
+    }
+    void Return()
+    {
+        Panel.SetActive(false);
+        MainPanel.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Collection/Watch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Watch : MonoBehaviour
+{
+    public Transform target;//获取旋转目标
+
+    private float mouse_x;
+
+    private float mouse_y;
+
+    private void LateUpdate() //摄像机围绕目标旋转操作
+    {
+        float mouse_x = Input.GetAxis("Mouse X");//获取鼠标X轴增量
+        Debug.Log(mouse_x);
+        float mouse_y = -Input.GetAxis("Mouse Y");//获取鼠标Y轴增量
+
+        if (Input.GetMouseButton(1))
+        {
+            transform.RotateAround(target.transform.position, Vector3.up, mouse_x * 5);
+            transform.RotateAround(target.transform.position, transform.right, mouse_y * 5);
+        }
+    }  
+}
